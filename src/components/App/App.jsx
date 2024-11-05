c8aeeaca736072b3049b2615f8921b2f757f72cc
Martin Young <MartingYoung@hotmail.com>
2024-11-05 09:16:17 -0600

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,36 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+
+import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
+
+import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="history" element={<SiteHistory />} />
+          <Route path="mission" element={<SiteMission />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
